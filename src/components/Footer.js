54363669a9525c4fb218500c61d5d1cf35d98b7e
LordEra <marvinezenwekwe@gmail.com>
2021-10-24 01:49:18 +0100

    Perfect Craft

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = ({count, setCount}) => {
         <footer>
             <button className="placid-button mace" type="submit" onClick={() => {setCount(count - 1)}}> Previous </button>
             <button className="placid-button" type="submit" onClick={() => {setCount(count + 1)}}> Next </button>
+            <p id="evolution"> © 2021 PhotoRama. All rights reserved.</p>
         </footer>
     );
 }
